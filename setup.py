15b1d8518426ff9582c012d478d090d72e086d53
Florian Fervers <florian.fervers@gmail.com>
2023-12-14 15:06:33 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="einx",
-    version="0.0.1",
+    version="0.0.2",
     python_requires=">=3",
     description="Tensor Operations Expressed in Einstein-Inspired Notation",
     author="Florian Fervers",
