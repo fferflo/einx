37bc0d86e938c1805e41f640a07afe167de918c5
Florian Fervers <florian.fervers@gmail.com>
2024-01-17 14:45:31 +0100

    Update python_requires in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="einx",
     version="0.1.0",
-    python_requires=">=3",
+    python_requires=">=3.8",
     description="Tensor Operations Expressed in Einstein-Inspired Notation",
     author="Florian Fervers",
     author_email="florian.fervers@gmail.com",
