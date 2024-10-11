95e2d3f969afd68e57850e14165c02b7ee7fdd94
Florian Fervers <florian.fervers@gmail.com>
2024-10-11 10:00:18 +0200

    Remove unnecessary reshapes

--- einx/backend/_mlx.py
+++ einx/backend/_mlx.py
@@ -61,7 +61,7 @@ def create():
         def reshape(tensor, shape):
             if einx.tracer.is_scalar(tensor):
                 tensor = tmx.array(tensor)
-            return op.reshape(tmx.reshape)(tensor, list(to_tuple(shape)))
+            return op.reshape(tmx.reshape)(tensor, to_tuple(shape))
 
         transpose = op.transpose(tmx.transpose)
         broadcast_to = op.broadcast_to(tmx.broadcast_to)
